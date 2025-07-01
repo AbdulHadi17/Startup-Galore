2bf885c18c2a9f91d6736f3ca9140abd9bf2cfd2
AbdulHadi17 <hadisaqib2048@gmail.com>
2025-07-01 17:59:42 +0500

    Views synced with DB

--- src/components/view.tsx
+++ src/components/view.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Ping from './ping'
 import { client } from '@/sanity/lib/client'
 import { STARTUP_VIEWS_QUERY } from '@/sanity/lib/queries'
+import { writeClient } from '@/sanity/lib/write-client'
+import { after} from 'next/server';
 
 const View = async ({id}:{id:string}) => {
 
@@ -10,6 +12,10 @@ const View = async ({id}:{id:string}) => {
   .fetch(STARTUP_VIEWS_QUERY, { id });
 
   const totalViews = result?.views ?? 0;
+
+  
+  after(async ()=> await writeClient.patch(id).set({views: totalViews+1}).commit()
+)
   
   return (
     <div className='flex justify-end items-center mt-5 fixed bottom-3 right-3'>
